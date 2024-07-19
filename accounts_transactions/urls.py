34448395a4a5e4308023cea597a2ac84743fa6f0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-19 16:10:40 +0000

    refactor: Update to use keyword arguments and CustomDefaultRouter directly

--- accounts_transactions/urls.py
+++ accounts_transactions/urls.py
@@ -1,7 +1,28 @@
-from famtrust.urls import router
-from .views import AccountsViewSet, TransactionsViewSet
+from famtrust.utils import CustomDefaultRouter
+from .views import (
+    AccountViewSet,
+    FamilyAccountViewSet,
+    FundRequestViewSet,
+    SubAccountViewSet,
+    TransactionViewSet,
+)
 
-router.register("accounts", AccountsViewSet, basename="account")
-router.register("transactions", TransactionsViewSet, basename="transaction")
-
+router = CustomDefaultRouter()
+router.register(
+    prefix="sub-accounts", viewset=SubAccountViewSet, basename="sub-account"
+)
+router.register(
+    prefix="transactions", viewset=TransactionViewSet, basename="transaction"
+),
+router.register(
+    prefix="family-accounts",
+    viewset=FamilyAccountViewSet,
+    basename="family-account",
+)
+router.register(prefix="accounts", viewset=AccountViewSet, basename="account")
+router.register(
+    prefix="fund-requests",
+    viewset=FundRequestViewSet,
+    basename="fund-request",
+)
 urlpatterns = router.urls
