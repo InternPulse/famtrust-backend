fa29f889c0fe1cda7ac9c4e0719f3ec01f3a848f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-19 15:49:31 +0000

    refactor: Update to use the CustomDefaultRouter and fix styling

--- family_memberships/urls.py
+++ family_memberships/urls.py
@@ -1,8 +1,15 @@
-from famtrust.urls import router
+from famtrust.utils import CustomDefaultRouter
 
 from .views import FamilyGroupsViewSet, MembershipsViewSet
 
-router.register("family-groups", FamilyGroupsViewSet, basename="family-group")
-router.register("memberships", MembershipsViewSet, basename="membership")
+router = CustomDefaultRouter()
+router.register(
+    prefix="family-groups",
+    viewset=FamilyGroupsViewSet,
+    basename="family-group",
+)
+router.register(
+    prefix="memberships", viewset=MembershipsViewSet, basename="membership"
+)
 
 urlpatterns = router.urls
