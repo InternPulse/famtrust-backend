b5c871c556abde9fa6ad046e9e174c308035b3bf
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-25 13:44:47 +0000

    style: Update styling

--- family_memberships/admin.py
+++ family_memberships/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from .models import FamilyGroup, FamilyMembership
+
+from .models import (
+    FamilyGroup,
+    FamilyMembership,
+)
+
 
 class FamilyGroupAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'created_at', 'updated_at', 'owner_id')
-    search_fields = ('name', 'description')
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+        "owner_id",
+    )
+    search_fields = ("name", "description")
+
 
 class FamilyMembershipAdmin(admin.ModelAdmin):
-    list_display = ('membership_id', 'user_id', 'family_group', 'joined_at')
-    search_fields = ('user_id', 'family_group__name')
+    list_display = ("id", "user_id", "family_group", "joined_at")
+    search_fields = ("user", "family_group__name")
+
 
 admin.site.register(FamilyGroup, FamilyGroupAdmin)
 admin.site.register(FamilyMembership, FamilyMembershipAdmin)
