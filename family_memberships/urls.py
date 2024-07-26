c6327d78324d94954075b8f4ec84ec577a371bd4
Blessing Onyi <meetonyinyechukwu@gmail.com>
2024-07-26 21:02:19 +0100

    committing file changes

--- family_memberships/urls.py
+++ family_memberships/urls.py
@@ -4,23 +4,11 @@ from famtrust.utils import CustomDefaultRouter
 from .views import FamilyGroupViewSet, FamilyMembershipViewSet
 
 router = CustomDefaultRouter()
-
-
-
-"""
-Register the FamilyGroupViewSet with the router
-"""
 router.register(
     prefix="family-groups",
     viewset=FamilyGroupViewSet,
     basename="family-group",
 )
-
-
-
-"""
-Register the MembershipViewSet with the router
-"""
 router.register(
     prefix="family-memberships",
     viewset=FamilyMembershipViewSet,
--- famtrust/settings.py
+++ famtrust/settings.py
@@ -22,9 +22,9 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # "accounts",
-    # "transactions",
+    "accounts",
     "family_memberships",
+    "transactions",
     "drf_spectacular",
     "drf_spectacular_sidecar",
     "corsheaders",
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "drf_yasg",
 ]
 
 MIDDLEWARE = [
@@ -94,7 +93,6 @@ else:
             "PORT": os.environ.get("DB_PORT"),
         }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -168,12 +166,14 @@ REST_FRAMEWORK = {
 
 API_VERSION = os.environ.get("API_VERSION", "v1")
 
-DESCRIPTION = """This microservice hosts the API endpoints related to Accounts
-              and transactions for the FamTrust platform.
-              """
+DESCRIPTION = """This microservice hosts the API endpoints related to
+account-related operations, family-related operations, and transactions.
+"""
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "FamTrust - Accounts and Transactions Microservice",
+    "TITLE": (
+        "FamTrust - Family Management, Accounts & Transactions Microservice"
+    ),
     "DESCRIPTION": DESCRIPTION,
     "VERSION": API_VERSION,
     "SERVE_INCLUDE_SCHEMA": False,
