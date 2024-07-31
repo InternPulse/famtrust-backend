4ccadf6bbeddd726bc2cd60b17e1e0844a226bd0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-31 19:16:32 +0000

    refactor: Update permissions.py to handle authorizations well

--- famtrust/permissions.py
+++ famtrust/permissions.py
@@ -3,12 +3,34 @@ This module defines permissions that ensure that users have the required
 privileges before performing an action
 """
 
+from django.utils.translation import gettext_lazy as _
 from rest_framework import (
     permissions,
     status,
 )
 
-from famtrust import utils
+from famtrust import models, utils
+
+
+class IsObjectOwnerOrCreator(permissions.BasePermission):
+    """
+    Verifies that the user making the change is the owner or creator of the
+    object.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        user = request.ft_user
+        if hasattr(obj, "requested_by"):
+            return user.id == obj.requested_by  # used for fund requests
+
+        if hasattr(obj, "created_by"):
+            return user.id == obj.created_by
+
+        if hasattr(obj, "owner_id"):
+            return user.id == obj.owner_id
 
 
 class IsAuthenticatedWithUserService(permissions.BasePermission):
@@ -18,18 +40,11 @@ class IsAuthenticatedWithUserService(permissions.BasePermission):
         return hasattr(request, "ft_user")
 
 
-class IsSubAccountOwnerOrCreator(permissions.BasePermission):
+class IsSubAccountOwnerOrCreator(IsObjectOwnerOrCreator):
     """
     Verifies that the user making the change is the owner
     or creator of the account.
     """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        user_id = request.ft_user.get("id")
-        return user_id == obj.owner_id or user_id == obj.created_by
 
 
 class IsFamilyAccountCreatorOrAdmin(permissions.BasePermission):
@@ -42,28 +57,21 @@ class IsFamilyAccountCreatorOrAdmin(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        user = request.ft_user
-        user_id = user.get('id')
-        if user_id != str(obj.created_by):
+        user: models.User = request.ft_user
+        if user.id != obj.created_by:
             raise utils.HTTPException(
                 detail={
-                    "error": "You are not authorized to perform this action."
+                    "error": _(
+                        "You are not authorized to perform this "
+                        "action."
+                    )
                 },
                 status_code=status.HTTP_403_FORBIDDEN,
             )
 
-        return True
 
-
-class IsFundRequestOwnerOrCreator(permissions.BasePermission):
+class IsFundRequestOwnerOrCreator(IsObjectOwnerOrCreator):
     """
     Verifies that the user making the change is the owner
     or creator of the fund request.
     """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        user_id = request.ft_user.get("id")
-        return user_id == obj.requested_by or user_id == obj.created_by
